1d683179a147a61f238724aaff8d8331919cb9b8
isaiah <greatpup1940@gmail.com>
2021-01-30 15:44:13 -0500

    Added zendesk widget to the app

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,11 +4,30 @@ import React from 'react';
 
 import Navbar from './components/Navbar';
 
+import Zendesk from 'react-zendesk';
+
+const setting = {
+  color: {
+    theme: '#006ab3',
+  },
+  launcher: {
+    chatLabel: {
+      'en-US': 'Need Help?',
+    },
+  },
+  contactForm: {
+    fields: [
+      { id: 'description', prefill: { '*': 'My pre-filled description' } },
+    ],
+  },
+};
+
 export default function index({ children }) {
   return (
     <div>
       <Navbar />
       <main>{children}</main>
+      <Zendesk zendeskKey={process.env.REACT_APP_ZENDESK_KEY} {...setting} />
     </div>
   );
 }
