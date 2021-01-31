8bf43fa0ca331be5b194603d279d071c3e5d0335
joeywire <61367542+joeywire@users.noreply.github.com>
2021-01-31 16:52:05 -0600

    added ll info

--- src/components/pages/Home/components/LandlordHome.js
+++ src/components/pages/Home/components/LandlordHome.js
@@ -19,10 +19,17 @@ const LandLordHome = ({ currentUser }) => {
       <div>
         Landlord features (from trello):
         <ul>
-          <li>Submit name, contact info, property info, and tenant info</li>
-          <li>Submit Request for rent assistance</li>
+          <li>
+            Submit name, contact info, property info, and tenant info (via
+            profile portal?)
+          </li>
+          <li>Submit Request for rent assistance (via form)</li>
           <li>See status of Request</li>
           <li>See Requests for info from my tenants</li>
+          <li>
+            Question?? are RFI's from tenants standard or are these custom
+            messages that need to be handled in BE?
+          </li>
         </ul>
       </div>
       <p>
