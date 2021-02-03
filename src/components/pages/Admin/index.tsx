ecd497fec45e897d62b408daebe88e6bea0518ae
Christopher Harwell <chrisharwell72@gmail.com>
2021-02-03 16:57:21 -0500

    Adds route for request table to admin home page

--- src/components/pages/Admin/index.tsx
+++ src/components/pages/Admin/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import UsersTable from './components/UsersTable';
-
+import { Route } from 'react-router-dom';
 import styles from '../../../styles/pages/admin.module.css';
+import RequestsTable from './components/RequestsTable';
 
 export default function Index() {
   return (
     <div className={styles.container}>
       <div className={styles.table}>
+        <Route exact path="admin/requests">
+          <RequestsTable />
+        </Route>
         <UsersTable />
       </div>
     </div>
