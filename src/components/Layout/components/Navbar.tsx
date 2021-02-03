aaf5029379e4f58e96b0786949bbdaea46ae0400
Christopher Harwell <chrisharwell72@gmail.com>
2021-02-03 16:56:48 -0500

    Adds link to Requests page to nav bar

--- src/components/Layout/components/Navbar.tsx
+++ src/components/Layout/components/Navbar.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import styles from '../../../styles/Layout/navbar.module.css';
 
 import { useOktaAuth } from '@okta/okta-react';
 
+import { useSelector } from 'react-redux';
+
+import RequestsTable from '../../pages/Admin/components/RequestsTable';
+
 function Navbar() {
   const { authState, authService } = useOktaAuth();
+
+  const currentUser = useSelector(state => state.user.currentUser);
 
   const redirectToHome = () => {
     if (authState.isAuthenticated) {
@@ -18,6 +24,14 @@ function Navbar() {
 
   const redirectToLogin = () => {
     history.push('/login');
+  };
+
+  const redirectToRequestsPage = () => {
+    return (
+      <Link to="admin/requests">
+        <h2>Requests</h2>
+      </Link>
+    );
   };
 
   const history = useHistory();
@@ -31,7 +45,11 @@ function Navbar() {
       <div onClick={redirectToHome} className={styles.logo}>
         <h2>RAP</h2>
       </div>
-
+      <div>
+        {currentUser.role === 'admin'
+          ? redirectToRequestsPage()
+          : 'You cant see this'}
+      </div>
       <ul className={styles.navActions}>
         {authState.isAuthenticated ? (
           <li onClick={handleLogout}>Logout</li>
