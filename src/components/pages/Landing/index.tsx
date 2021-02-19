d877720a2d1aed3141b198fd6814d764fae37b31
Isaiah <greatpup1940@gmail.com>
2021-02-19 18:45:38 -0500

    Added background image with gradient

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import styles from '../../../styles/pages/landing.module.css';
+
 export default function Index() {
-  return <div></div>;
+  return (
+    <div>
+      <header className={styles.header}></header>
+    </div>
+  );
 }
