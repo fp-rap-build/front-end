2e1ea697ef304fce6772cc72e6be40dfd062657b
Chris Harwell <chrisharwell72@gmail.com>
2021-03-09 11:25:50 -0500

    Adding state to checklist, probably going to create a custom hook for handling state for checklist accross components

--- src/components/modals/Checklist/Checklist.tsx
+++ src/components/modals/Checklist/Checklist.tsx
@@ -5,9 +5,15 @@ import React, { useState } from 'react';
 const Checklist = () => {
   // state for checklist modal
   const [isChecklistModalVisible, setisChecklistModalVisible] = useState(false);
+  const [isCheckboxModalChecked, setIsCheckboxChecked] = useState(false);
+
   const showChecklistModal = () => setisChecklistModalVisible(true);
   const handle_Checklist_Modal_Ok = () => setisChecklistModalVisible(false);
   const handle_Checklist_Modal_Cancel = () => setisChecklistModalVisible(false);
+  const handle_Checklist_Modal_State = () => {
+    // TODO: add if statement that checks if all checkboxes are checked
+    setIsCheckboxChecked(false);
+  };
 
   const checkList = (
     <>
@@ -15,7 +21,30 @@ const Checklist = () => {
       <Checkbox>Approved by Program Manager</Checkbox>
       <Checkbox>Approved by Head Accountant</Checkbox>
       <Checkbox>Approved by Book keeper</Checkbox>
-      <Checkbox>Approved by Account Manager</Checkbox>
+    </>
+  );
+
+  const options = [
+    {
+      label: 'Approved by Account Manager',
+      value: 'Approved by Account Manager',
+    },
+    {
+      label: 'Approved by Promgram Manager',
+      value: 'Approved by Promgram Manager',
+    },
+    {
+      label: 'Approved by Head Accountant',
+      value: 'Approved by Head Accountant',
+    },
+    { label: 'Approved by Book keeper', value: 'Approved by Book keeper' },
+  ];
+  const checkList2 = (
+    <>
+      <Checkbox.Group
+        options={options}
+        onChange={handle_Checklist_Modal_State}
+      />
     </>
   );
   return (
