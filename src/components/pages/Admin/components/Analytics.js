f7c78d2df955389badefc49cff8d5c709abaac91
Isaiah <greatpup1940@gmail.com>
2021-03-19 17:43:21 -0400

    Added some basic cards for scaffolding

--- src/components/pages/Admin/components/Analytics.js
+++ src/components/pages/Admin/components/Analytics.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import styles from '../../../../styles/pages/admin.module.css';
+
 const Analytics = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className={styles.cardsContainer}>
+        <Card value="18" title="Families served" color="#006ab3" />
+        <Card value="62" title="People served" color="#006ab3" />
+        <Card value="$ 1000" title="Budget" color="#006ab3" />
+      </div>
+    </div>
+  );
+};
+
+const Card = props => {
+  return (
+    <div style={{ backgroundColor: props.color }} className={styles.card}>
+      <h3 className={styles.value}>{props.value}</h3>
+      <h4 className={styles.title}>{props.title}</h4>
     </div>
   );
 };
