d9db62f9660d5d6d2c055e63be18b6d66c2275a4
Chris Harwell <chrisharwell72@gmail.com>
2021-03-20 12:26:49 -0400

    Fixes a bug i introduced

--- src/components/pages/Admin/components/Analytics.js
+++ src/components/pages/Admin/components/Analytics.js
@@ -4,22 +4,33 @@ import { axiosWithAuth } from '../../../../api/axiosWithAuth';
 
 const Analytics = () => {
   const [peopleServed, setPeopleServed] = useState();
+  const [familiesServed, setFamiliesServed] = useState();
 
   function getPeopleServed() {
     axiosWithAuth()
       .get('/analytics')
       .then(res => {
-        const data = res.data.sumRequests;
-        return setPeopleServed(data[0].count);
+        const peopleServed = res.data.sumPeopleServed;
+        const familiesServed = res.data.sumFamiliesServed;
+
+        const numPeopleServed = setPeopleServed(peopleServed[0].count);
+        const numFamiliesServed = setFamiliesServed(familiesServed[0].count);
+
+        return [numPeopleServed, numFamiliesServed];
       })
       .catch(err => console.error(err));
   }
+
   return (
     <div>
       <div className={styles.cardsContainer}>
         {getPeopleServed()}
         <Card value="64" title="Families served" color="#006ab3" />
-        <Card value={peopleServed} title="People served" color="#006ab3" />
+        <Card
+          value={getPeopleServed[0]}
+          title="People served"
+          color="#006ab3"
+        />
         <Card value="$ 1000" title="Budget" color="#006ab3" />
       </div>
     </div>
