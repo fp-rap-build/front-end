3e9f7a3a6a52acb36b1c5073069597371d1ecfce
joeywire <61367542+joeywire@users.noreply.github.com>
2021-04-17 12:09:22 -0500

    updated checklist layout and built placeholder for post-approval list

--- src/components/pages/Requests/components/RequestInformation/components/Checklist.tsx
+++ src/components/pages/Requests/components/RequestInformation/components/Checklist.tsx
@@ -1,9 +1,28 @@
-import { Checkbox } from 'antd';
-import React from 'react';
+import styles from '../../../../../../styles/pages/request.module.css';
+
+import { Checkbox, Typography } from 'antd';
+const { Title } = Typography;
+
+const marginFix = {
+  marginLeft: '0',
+};
 
 const Checklist = ({ handleCheckboxChange, request }) => {
   return (
-    <div>
+    <div className={styles.checklistsContainer}>
+      <PreApprovalChecklist
+        handleCheckboxChange={handleCheckboxChange}
+        request={request}
+      />
+      <PostApprovalChecklist />
+    </div>
+  );
+};
+
+const PreApprovalChecklist = ({ handleCheckboxChange, request }) => {
+  return (
+    <div className={styles.checklist}>
+      <Title level={5}>Pre-Approval Checklist</Title>
       <Checkbox
         checked={request.verifiedDocuments}
         name="verifiedDocuments"
@@ -11,14 +30,29 @@ const Checklist = ({ handleCheckboxChange, request }) => {
       >
         All documents received and verified
       </Checkbox>
-      <br />
       <Checkbox
         checked={request.pmApproval}
         name="pmApproval"
         onChange={handleCheckboxChange}
+        style={marginFix}
       >
         Approved by program manager
       </Checkbox>
+      <Checkbox style={marginFix}>
+        Rent payment ledger received from landlord
+      </Checkbox>
+    </div>
+  );
+};
+
+const PostApprovalChecklist = props => {
+  return (
+    <div className={styles.checklist}>
+      <Title level={5}>Post-Approval Checklist</Title>
+      <Checkbox>Placeholder</Checkbox>
+      <Checkbox style={marginFix}>Placeholder</Checkbox>
+      <Checkbox style={marginFix}>Placeholder</Checkbox>
+      <Checkbox style={marginFix}>Placeholder</Checkbox>
     </div>
   );
 };
